da8759d8d11bd7e47ca3a079d56fe68d03700f6b
anishvk <anishvk@umich.edu>
2022-06-27 22:07:14 -0400

    removed unnecessary wrappers

--- scraper.py
+++ scraper.py
@@ -5,50 +5,35 @@ source = requests.get('https://pokemondb.net/pokedex/all').text
 soup = BeautifulSoup(source, 'lxml')
 
 table = soup.find('tbody')
+
 for entry in table.find_all('tr'):
-    identifier = entry.find('td', {'class': 'cell-num cell-fixed'})
+    allInfo = entry.find_all('td')
 
-    # imageWrapper = identifier.find('span', {'class': 'infocard-cell-img'})
-    # image = imageWrapper.find('span', {'class': 'img-fixed icon-pkmn'}).get('data-src')
-    # print(image)
-
-    number = identifier.find('span', {'class': 'infocard-cell-data'}).text
-
-    nameWrapper = entry.find('td', {'class': 'cell-name'})
-    link = nameWrapper.find('a')['href']
-    name = nameWrapper.find('a').text
+    number = allInfo[0].find('span', {'class': 'infocard-cell-data'}).text
+    linkExtension = allInfo[1].find('a')['href']
+    name = allInfo[1].find('a').text
     try:
-        form = nameWrapper.find('small').text
+        form = allInfo[1].find('small').text
     except:
         form = 'N/A'
 
-    typingsWrapper = entry.find('td', {'class': 'cell-icon'})
-    typings = typingsWrapper.find_all('a')
+    typings = allInfo[2].find_all('a')
     type1 = typings[0].text
     try:
         type2 = typings[1].text
     except:
         type2 = 'N/A'
     
-    bst = entry.find('td', {'class': 'cell-total'}).text
-    allStats = entry.find_all('td', {'class': 'cell-num'})
-    hp = allStats[0].text
-    attack = allStats[1].text
-    defense = allStats[2].text
-    sp_attack = allStats[3].text
-    sp_defense = allStats[4].text
-    speed = allStats[5].text
+    bst = allInfo[3].text
+    hp = allInfo[4].text
+    attack = allInfo[5].text
+    defense = allInfo[6].text
+    sp_attack = allInfo[7].text
+    sp_defense = allInfo[8].text
+    speed = allInfo[9].text
 
     print(number, name, form, type1, type2, bst, hp, attack, defense, sp_attack, sp_defense, speed)
     print()
-
-
-
-
-
-
-
-
 
 
 
