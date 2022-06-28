c3fd605c7b4558e7fe303de4a2bc53f295f265e8
anishvk <anishvk@umich.edu>
2022-06-28 00:26:48 -0400

    deleted unnecessary try catch block +  typing links

--- scraper.py
+++ scraper.py
@@ -3,65 +3,63 @@ import requests
 import csv
 import urllib.request
 
+# get the HTML from the URL
 source = requests.get('https://www.serebii.net/pokemon/nationalpokedex.shtml').text
 soup = BeautifulSoup(source, 'lxml')
+
+# initiliaze the csv file
 csvFile = open('pokemonInfo.csv', 'w')
 csvWriter = csv.writer(csvFile)
-csvWriter.writerow(['Number', 'Image URL', 'Name', 'Typings', 'Abilities', 'Base Stat Total', 'HP', 'Attack', 'Defense', 'Sp.Attack', 'Sp.Defense', 'Speed'])
+csvWriter.writerow(['Number', 'Name', 'Typings', 'Abilities', 'Base Stat Total', 'HP', 'Attack', 'Defense', 'Sp.Attack', 'Sp.Defense', 'Speed'])
 
+# get the table
 table = soup.find('table', class_='dextable')
 table = table.find_all('tr')
+
+# creates a list of all image links
 allImageURLs = []
 
+# loops through the table and skips every other row
 for i in range(0, len(table), 2):
+    # skips the first row of the table 
     if i == 0:
         continue
 
+    # compiles all pokemon info into a list
     allInfo = table[i].find_all('td', {'class': 'fooinfo'})
-    try:
-        number = allInfo[0].text.strip()
-        imageURL = allInfo[1].find('img')['src']
-        name = allInfo[2].text.strip()
 
-        typingLinks = allInfo[3].find_all('a')
-        for i in range (len(typingLinks)):
-            typingLinks[i] = typingLinks[i].get('href')
+    # gets the attributes of the pokemon
+    number = allInfo[0].text.strip()
+    name = allInfo[2].text.strip()
 
-        typings = typingLinks
-        for i in range (len(typings)):
-            typings[i] = typings[i][14:].capitalize()
+    # appends the typings as a list
+    typings = allInfo[3].find_all('a')
+    for i in range (len(typings)):
+        typings[i] = typings[i].get('href')[14:].capitalize()
 
-        abilities = allInfo[4].find_all('a')
-        for i in range (len(abilities)):
-            abilities[i] = abilities[i].text.strip()
-        
-        hp = allInfo[5].text.strip()
-        attack = allInfo[6].text.strip()
-        defense = allInfo[7].text.strip()
-        sp_attack = allInfo[8].text.strip()
-        sp_defense = allInfo[9].text.strip()
-        speed = allInfo[10].text.strip()
-
-    except:
-        number = 'N/A'
-        imageURL = 'N/A'
-        name = 'N/A'
-        typings = 'N/A'
-        abilities = 'N/A'
-        hp = 'N/A'
-        attack = 'N/A'
-        defense = 'N/A'
-        sp_attack = 'N/A'
-        sp_defense = 'N/A'
-        speed = 'N/A'
-        
-    imageURLfull = 'https://www.serebii.net' + imageURL
+    # appends the abilities as a list
+    abilities = allInfo[4].find_all('a')
+    for i in range (len(abilities)):
+        abilities[i] = abilities[i].text.strip()
     
+    # gets the base stats
+    hp = allInfo[5].text.strip()
+    attack = allInfo[6].text.strip()
+    defense = allInfo[7].text.strip()
+    sp_attack = allInfo[8].text.strip()
+    sp_defense = allInfo[9].text.strip()
+    speed = allInfo[10].text.strip()
     bst = int(hp) + int(attack) + int(defense) + int(sp_attack) + int(sp_defense) + int(speed)
-    allImageURLs.append(imageURLfull)
-    print(number, imageURLfull, name, typings, abilities, bst, hp, attack, defense, sp_attack, sp_defense, speed)
-    csvWriter.writerow([number, imageURLfull, name, typings, abilities, bst, hp, attack, defense, sp_attack, sp_defense, speed])
 
+    # gets the image link
+    partialURL = allInfo[1].find('img')['src']
+    imageURL = 'https://www.serebii.net' + partialURL
+    allImageURLs.append(imageURL)
+    
+    # print(number, name, typings, abilities, bst, hp, attack, defense, sp_attack, sp_defense, speed)
+    csvWriter.writerow([number, name, typings, abilities, bst, hp, attack, defense, sp_attack, sp_defense, speed])
+
+# downloads all the pokemon sprites
 for i in range(len(allImageURLs)):
     if len(str(i + 1)) < 3:
         num = '0' * (3 - len(str(i + 1))) + str(i + 1)
