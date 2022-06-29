e860e030030038ab90f537e79322f1fe30509c54
anishvk <anishvk@umich.edu>
2022-06-29 12:30:46 -0400

    added pokemon models to bio

--- components/bio/pokemonBio.js
+++ components/bio/pokemonBio.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const PokemonBio = ({ pokemon }) => {
   return (
     <View style={styles.titleContainer}>
       <Text style={styles.titleText}>{pokemon.name}</Text>
+
+      <View style={styles.modelContainer}>
+        <Image style={styles.model} source={pokemon.model} />
+      </View>
     </View>
   )
 }
@@ -21,6 +25,19 @@ const styles = StyleSheet.create({
     fontFamily: 'Ubuntu_700Bold',
     fontSize: '30',
   },
+
+  modelContainer: {
+    marginTop: '8%',
+    borderWidth: 1,
+    borderColor: '#ffffff',
+    borderRadius: 40,
+    padding: 20,
+  },
+
+  model: {
+    width: 160,
+    height: 160,
+  }
 });
 
 export default PokemonBio
